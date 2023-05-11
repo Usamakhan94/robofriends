ec8c7bd904412a4434630a9632a32aa64b6ee029
Usama <usama_khan1994@live.com>
2023-05-11 18:19:40 +0500

    Updates

---
+++ precache-manifest.eae683ff01507eeefc78211ef305d61c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "89184355a6a210462d11",
+    "url": "/robofriends/static/css/main.2c0b43dc.chunk.css"
+  },
+  {
+    "revision": "89184355a6a210462d11",
+    "url": "/robofriends/static/js/main.89184355.chunk.js"
+  },
+  {
+    "revision": "fdf175286c6cdd58696b",
+    "url": "/robofriends/static/js/1.fdf17528.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "29e78f978c7435205c4ec9b5845a992f",
+    "url": "/robofriends/index.html"
+  }
+];
